46bc46bbddab92495fecd1d1d62a05c18c565eff
Yuraima Estevez <yurm04@gmail.com>
2019-05-12 16:06:51 -0400

    updated config

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ module.exports = {
     photo: '/photo.jpg',
     bio: 'Yuraima Estevez is a software engineer at The New York Times.  She cares deeply about building open source tools, empathetic web design and development, and finding the best tacos in whatever city she happens to be.',
     contacts: {
-      email: 'yurm04@gmail.co,',
+      email: 'yurm04@gmail.com',
       twitter: 'Yurm04',
       github: 'yurm04/',
       telegram: '',
