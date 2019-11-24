79a001d8e23a54d27312dcc8cc459325969e07fa
Yuraima <yurm04@gmail.com>
2019-11-23 21:23:03 -0500

    only show menu if items exist

--- src/components/Sidebar/Menu/Menu.js
+++ src/components/Sidebar/Menu/Menu.js
@@ -10,22 +10,26 @@ type Props = {
   }[]
 };
 
-const Menu = ({ menu }: Props) => (
-  <nav className={styles['menu']}>
-    <ul className={styles['menu__list']}>
-      {menu.map((item) => (
-        <li className={styles['menu__list-item']} key={item.path}>
-          <Link
-            to={item.path}
-            className={styles['menu__list-item-link']}
-            activeClassName={styles['menu__list-item-link--active']}
-          >
-            {item.label}
-          </Link>
-        </li>
-      ))}
-    </ul>
-  </nav>
-);
+const Menu = ({ menu }: Props) => {
+  if (menu) return null;
+
+  return (
+    <nav className={styles['menu']}>
+      <ul className={styles['menu__list']}>
+        {menu.map((item) => (
+          <li className={styles['menu__list-item']} key={item.path}>
+            <Link
+              to={item.path}
+              className={styles['menu__list-item-link']}
+              activeClassName={styles['menu__list-item-link--active']}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Menu;
