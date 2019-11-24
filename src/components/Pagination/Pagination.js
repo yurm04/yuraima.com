87f220fff04f1b99fae8635ae685e31a0346c53c
Yuraima <yurm04@gmail.com>
2019-11-23 21:12:46 -0500

    only show pagination if needed

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -19,27 +19,19 @@ const Pagination = ({
   nextPagePath,
   hasNextPage,
   hasPrevPage
-}: Props) => {
-  const prevClassName = cx({
-    'pagination__prev-link': true,
-    'pagination__prev-link--disable': !hasPrevPage
-  });
-
-  const nextClassName = cx({
-    'pagination__next-link': true,
-    'pagination__next-link--disable': !hasNextPage
-  });
-
-  return (
+}: Props) => (
     <div className={styles['pagination']}>
-      <div className={styles['pagination__prev']}>
-        <Link rel="prev" to={prevPagePath} className={prevClassName}>{PAGINATION.PREV_PAGE}</Link>
-      </div>
-      <div className={styles['pagination__next']}>
-        <Link rel="next" to={nextPagePath} className={nextClassName}>{PAGINATION.NEXT_PAGE}</Link>
-      </div>
+      {hasPrevPage && (
+        <div className={styles['pagination__prev']}>
+          <Link rel="prev" to={prevPagePath} className={styles['pagination__prev-link']}>{PAGINATION.PREV_PAGE}</Link>
+        </div>
+      )}
+      {hasNextPage && (
+        <div className={styles['pagination__next']}>
+          <Link rel="next" to={nextPagePath} className={styles['pagination__next-link']}>{PAGINATION.NEXT_PAGE}</Link>
+        </div>
+      )}
     </div>
   );
-};
 
 export default Pagination;
