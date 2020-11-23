81f7e024bba02ff8d909b3f1b09f83306ae04d5c
Yuraima <yurm04@gmail.com>
2020-11-23 16:46:01 -0500

    update moving

--- src/pages/moving.js
+++ src/pages/moving.js
@@ -28,19 +28,19 @@ export default ({ location, data }) => {
     <Layout location={location} title={site.siteMetadata.title}>
       <Styled.h1 sx={headingStyle}>I'm Moving!</Styled.h1>
       <Styled.p>So everything in my apartment has to go. All things are in great condition and prices are listed. Open to delivering some things if you're not too far. Can strike a deal if you take more than one thing. You know how to get in touch with me if you got to this page...</Styled.p>
-      <ListItem name="Article couch" price={800} img={['couch1', 'couch2', 'couch3', 'couch4']}>
+      <ListItem name="Article couch" price={600} img={['couch1', 'couch2', 'couch3', 'couch4']}>
         <Styled.p>6ft. 11in. wide by 2ft. 11in. deep. One year old and still beautiful because 7 months of that year I was the only one sitting on it (s i g h. Pandemic life). <a href="https://www.article.com/product/1479/ceni-pyrite-gray-sofa">All in with tax and shipping it cost me $1200</a>. I love this couch, and I hate to sell it. But it'd be weird if I tried to take it with me on my cross-country travels...</Styled.p>
       </ListItem>
       <ListItem name="Bed frame, full" price={150} img={['bed1', 'bed2', 'bed3']} desc="82in. long, 56in. wide. Frabric headreast and slatted platform"/>
-      <ListItem name="Tuft and Needle mattress, full" price={350} img={['mattress1', 'mattress2']}/>
-      <ListItem name='43" TCL 4K Roku Smart TV' price={250} img={['tv1', 'tv2', 'tv3', 'tv4', 'tv5', 'tv6']} desc="Roku smart TV with all the apps. A year old and in perfect condition." />
+      <ListItem name="Tuft and Needle mattress, full" price={200} img={['mattress1', 'mattress2']}/>
+      {/* <ListItem name='43" TCL 4K Roku Smart TV' price={250} img={['tv1', 'tv2', 'tv3', 'tv4', 'tv5', 'tv6']} desc="Roku smart TV with all the apps. A year old and in perfect condition." /> */}
       {/* <ListItem name="White-ish rug" price={75} img={['rug1']}>
         <Styled.p>6' ish x 5' ish rug. This rug is absolutely ✨PRISTINE✨ due to this being a shoe-free household. You could eat off of this rug if you wanted to. I don't, because that's super weird, but what you do with this rug after it's yours is your business.</Styled.p>
       </ListItem> */}
       <ListItem name="Tall table with stools" price={100} img={['table1', 'table2', 'table3', 'table4']}/>
       <ListItem name="End table one" price={35} img={['endtable3', 'endtable4']}/>
       <ListItem name="End table two" price={20} img={['endtable1']}/>
-      {/* <ListItem name="End table/bar" img={['endtable2']} price={50} desc="I think this is an end table but it holds my liquor so it is, in spirit, a bar (pun intended)." /> */}
+      <ListItem name="End table/bar" img={['endtable2']} price={50} desc="I think this is an end table but it holds my liquor so it is, in spirit, a bar (pun intended)." />
       {/* <ListItem name="Floor Lamp" price={80} img={['lamp3']} desc="I have turned this lamp on exactly three times. Humble brag but I have a lot of light in my apartment so I never actually needed it. Will even throw in the mint condition lightbulb." /> */}
       <ListItem name="Table Lamp" price={50} img={['lamp1', 'lamp2']}/>
       {/* <ListItem name="Mirror" price={50} img={['mirror1', 'mirror2']}/> */}
